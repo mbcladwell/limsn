8fcf68208af8caa551aed5037f6c13adb12e47d9
mbcladwell <mbcladwell@stihie.net>
2021-06-22 08:01:36 -0400

    changed

--- limsn/postgres/create-db.sql
+++ limsn/postgres/create-db.sql
@@ -1,39 +1,3 @@
-REVOKE ALL ON DATABASE lndb FROM public;  -- see notes below!
-
-GRANT CONNECT ON DATABASE lndb TO ln_user;  -- others inherit
-
---\connect lndb  -- psql syntax
-
---I am naming the schema lims_nucleus (not lndb which would be confusing). Pick any name. Optionally make ln_admin the owner of the schema:
-
-CREATE SCHEMA lims_nucleus AUTHORIZATION ln_admin;
-
-ALTER DATABASE lndb SET search_path = lims_nucleus;  -- see notes
-
-ALTER ROLE ln_admin IN DATABASE lndb SET search_path = lims_nucleus; -- not inherited
-ALTER ROLE ln_user   IN DATABASE lndb SET search_path = lims_nucleus;
-
-GRANT USAGE  ON SCHEMA lims_nucleus TO ln_user;
-GRANT USAGE  ON SCHEMA lims_nucleus TO ln_admin;
-GRANT CREATE ON SCHEMA lims_nucleus TO ln_admin;
-
-ALTER DEFAULT PRIVILEGES FOR ROLE ln_admin
-GRANT SELECT, INSERT, UPDATE, DELETE, TRUNCATE  ON TABLES TO ln_admin;  
-
-ALTER DEFAULT PRIVILEGES FOR ROLE ln_user
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO ln_user;
-
-
-GRANT TEMPORARY on DATABASE lndb to ln_admin;
-GRANT TEMPORARY on DATABASE lndb to ln_user;
-
-
-set schema 'lims_nucleus';	
---CREATE EXTENSION pgcrypto;
-CREATE EXTENSION intarray;
---Once set up:
--- psql -U ln_admin -h 192.168.1.1 -d lndb
-
 
 
 DROP TABLE IF EXISTS config CASCADE;
