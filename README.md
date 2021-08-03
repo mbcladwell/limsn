602429fad097ce037a3ffe70af1f97b6f0d82930
mbcladwell <mbcladwell@stihie.net>
2021-08-03 15:30:12 -0400

    removed bootstrap which has autoreconf

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ How are registered users handled in software?
 plate/getpltforps?id=1 2nd 3rd table missing indentation
 
 Based on ln7 which run guile3.0.5/Datatables/bootstrap/postgres
+
+
+for make to work must modify load path
+export GUILE_LOAD_PATH="/home/mbc/projects/limsn/limsn/lib${GUILE_LOAD_PATH:+:}$GUILE_LOAD_PATH"
--- limsn.scm
+++ limsn.scm
@@ -17,6 +17,7 @@
   (gnu packages tls)
   (gnu packages libffi)
   (gnu packages crypto)
+  (guix git-download)
   (gnutls)
 )
 
@@ -25,11 +26,13 @@
     (name "limsn")
     (version "0.1.0")
     (source (origin
-              (method url-fetch)
-              (uri (string-append "mirror://gnu/artanis/artanis-0.5.tar.gz"))
+	      (method git-fetch)
+	   (uri (git-reference 
+		 (url "https://github.com/mbcladwell/limsn.git")
+		 (commit "0954e331ede375c4cdb59e9946bfb394a5090a8e")))    
               (sha256
                (base32
-                "1vk1kp2xhz35xa5n27cxlq9c88wk6qm7fqaac8rb0pb6k9pvsv7v"))
+                "1a5lbdby17vdvn67nqrm0k9zq8kpg4wi3s25v9bcn6wp6zxz1936"))
               (modules '((guix build utils)))
               ))
     (build-system gnu-build-system)
@@ -52,37 +55,8 @@
          (list (string-append "MOD_PATH=" scm)
                (string-append "MOD_COMPILED_PATH=" go)))
        #:test-target "test"
-       #:phases
-       (modify-phases %standard-phases
-         (add-after 'unpack 'patch-site-dir
-           (lambda* (#:key outputs #:allow-other-keys)
-             (substitute* "artanis/commands/help.scm"
-               (("\\(%site-dir\\)")
-                (string-append "\""
-                               (assoc-ref outputs "out")
-                               "/share/guile/site/3.0\"")))))
-      
-         (add-before 'install 'substitute-root-dir
-           (lambda* (#:key outputs #:allow-other-keys)
-             (let ((out  (assoc-ref outputs "out")))
-               (substitute* "Makefile"   ;ignore the execution of bash.bashrc
-                 ((" /etc/bash.bashrc") " /dev/null"))
-               (substitute* "Makefile"   ;set the root of config files to OUT
-                 ((" /etc") (string-append " " out "/etc")))
-               (mkdir-p (string-append out "/bin")) ;for the `art' executable
-               #t)))
-         (add-after 'install 'wrap-art
-           (lambda* (#:key inputs outputs #:allow-other-keys)
-             (let* ((out (assoc-ref outputs "out"))
-                    (bin (string-append out "/bin"))
-                    (scm (string-append out "/share/guile/site/3.0"))
-                    (go  (string-append out "/lib/guile/3.0/site-ccache")))
-               (wrap-program (string-append bin "/art")
-                 `("GUILE_LOAD_PATH" ":" prefix
-                   (,scm ,(getenv "GUILE_LOAD_PATH")))
-                 `("GUILE_LOAD_COMPILED_PATH" ":" prefix
-                   (,go ,(getenv "GUILE_LOAD_COMPILED_PATH"))))
-               #t))))))
+     
+       ))
     (synopsis "Microwell Plate management Software")
     (description "description")
     (home-page "http://www.labsolns.com/")
