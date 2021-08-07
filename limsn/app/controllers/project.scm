425dec1faaeeb62f0769d25f20ec7756ecdc2083
mbcladwell <mbcladwell@stihie.net>
2021-08-07 07:55:27 -0400

    change #:auth login uri

--- limsn/app/controllers/project.scm
+++ limsn/app/controllers/project.scm
@@ -48,7 +48,8 @@
 (project-define getall
 		(options #:conn #t 
 			 #:cookies '(names prjid sid)
-		;;	 #:with-auth "/login" )
+			 ;;	 #:with-auth "/login" )
+			 #:session #t
 			 #:with-auth  (get-redirect-uri "login"))
 			 (lambda (rc ) 
 			   (let* ( 
