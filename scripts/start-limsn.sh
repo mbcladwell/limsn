ca5f01b7bdf5482699f3547e0e0df9a0dfadc227
mbcladwell <mbcladwell@gmail.com>
2024-12-28 08:28:14 -0500

    updated .sh files with path-into-store

--- scripts/start-limsn.sh
+++ scripts/start-limsn.sh
@@ -3,4 +3,4 @@ PATH_INTO_STORE=path-into-store
 export LC_ALL="C"
 mkdir -p /var/tmp/limsn/tmp/cache
 cd $PATH_INTO_STORE/share/guile/site/3.0/limsn
-art work -h0.0.0.0 --config=$HOME/.config/limsn/artanis.conf
+art.in work -h0.0.0.0 --config=$HOME/.config/limsn/artanis.conf
