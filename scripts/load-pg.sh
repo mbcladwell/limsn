849a5bbdf22985b1bf800ab24487da64d5caa48b
mbcladwell <mbcladwell@gmail.com>
2024-12-29 11:22:17 -0500

    autocommit

--- scripts/load-pg.sh
+++ scripts/load-pg.sh
@@ -2,11 +2,11 @@
 
 PATH_INTO_STORE=pathintostore/share/guile/site/3.0/limsn/postgres
    
- owner=$1 
- psql -U $owner -h 127.0.0.1 postgres -a -f $PATH_INTO_STORE/initdba.sql
+ psql -U $1 -h 127.0.0.1 postgres -a -f $PATH_INTO_STORE/initdba.sql
  psql -U ln_admin -h 127.0.0.1 postgres -a -f $PATH_INTO_STORE/initdbb.sql
  psql -U ln_admin -h 127.0.0.1 -d lndb -a -f $PATH_INTO_STORE/initdbc.sql
  psql -U ln_admin -h 127.0.0.1 -d lndb -a -f $PATH_INTO_STORE/create-db.sql
  psql -U ln_admin -h 127.0.0.1 -d lndb -a -f $PATH_INTO_STORE/example-data.sql
 
 
+## "pathintostore"
