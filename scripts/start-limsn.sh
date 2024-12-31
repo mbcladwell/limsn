0c1943aa06431d9481631d1a590eea8269ae040c
mbcladwell <mbcladwell@gmail.com>
2024-12-31 08:55:57 -0500

    autocommit

--- scripts/start-limsn.sh
+++ scripts/start-limsn.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 PATH_INTO_STORE=$HOMEpathintostore
 export LC_ALL="C"
-mkdir -p /var/tmp/limsn/tmp/cache
+##mkdir -p /var/tmp/limsn/tmp/cache
+mkdir -p /tmp/limsn/tmp/cache/
 cd $PATH_INTO_STORE/share/guile/site/3.0/limsn
 art.in work -h0.0.0.0 --config=$HOME/.config/limsn/artanis.conf
