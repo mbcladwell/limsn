b483463abe359f85dcb19e5e845add63609db8ef
mbcladwell <mbcladwell@gmail.com>
2025-03-03 15:17:19 -0500

    autocommit

--- limsn/run-clean.sh
+++ limsn/run-clean.sh
@@ -7,6 +7,6 @@ export LC_ALL="C"
 #export GUILE_LOAD_PATH="/gnu/store/rfgcri126zxyd3kdxp23751pw18lrca4-artanis-1.0.0/share/guile/site/3.0${GUILE_LOAD_PATH:+:}$GUILE_LOAD_PATH"
 #export GUILE_DBD_PATH="/gnu/store/pg4pg9j78bxp6lvyc0dlmyf27q5rgzp5-guile-dbd-postgresql-2.1.8/lib${GUILE_DBD_PATH:+:}$GUILE_DBD_PATH"
 
-art.in work --config=/home/mbc/.config/limsn/artanis.conf
+art work --config=/home/mbc/.config/limsn/artanis.conf
 
 ## working with artanis 1.0.0 but not nss/nspr
